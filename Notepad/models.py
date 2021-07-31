ad708dc181021f5327665ade4ab2e0d0c789b7a5
smal1378 <smal1378@gmail.com>
2021-07-31 22:24:23 +0430

    backend completed login view

--- Notepad/models.py
+++ Notepad/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Notepad(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    
+
+class Page(models.Model):
+    master = models.ForeignKey(Notepad, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=50)
+    body = models.TextField()
--- Notepad/views.py
+++ Notepad/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 # Create your views here.
 
 
@@ -9,5 +13,49 @@ def home(request):
     
 
 def login(request):
-    return render(request, "Notepad/login.html")
-    
+    if request.is_ajax and request.method == "POST":
+        data = request.POST
+        if "action" in data and data["action"] == "login":
+            if 'username' in data and 'password' in data:
+                user = authenticate(request, 
+                                    username=data['username'],
+                                    password=data['password'])
+                if user:
+                    login(request, user)
+                    return JsonResponse({'res':'True'}])
+                else:
+                    return JsonResponse({"res":'False'})
+        elif 'action' in data and data['action'] == 'logout':
+            if request.user.is_authenticated:
+                logout(request)
+                return JsonResponse({'res':'True'}])
+        elif 'action' in data and data['action'] == 'signup':
+            if 'username' in data and 'password' in data and 'email' in data:
+                if User.objects.filter(username=data['username']).exists():
+                    return JsonResponse({'res':'False', 
+                                         'err':'1'})
+                try:
+                    validate_email(data['email'])
+                except ValidationError:
+                    return JsonResponse({'res':'False', 'err':'2'})
+                user = User.objects.create_user(data['username'], 
+                                                data['email'],
+                                                data['password'])
+                user.save()
+                return JsonResponse({'res':'True'})
+        elif 'action' in data and data['action'] == 'username_exists':
+            if 'username' in data and User.objects.filter(username=data['username']).exists():
+                return JsonResponse({'res':'True'})
+            else:
+                return JsonResponse({'res':'False'})
+    else:    
+        logged_in = False
+        username = "Guest"
+        if request.user.is_authenticated:
+            logged_in = True
+            username = request.user.username
+        return render(request, 
+                      "Notepad/login.html",
+                      {"logged_in": logged_in,
+                       "username": username})
+
